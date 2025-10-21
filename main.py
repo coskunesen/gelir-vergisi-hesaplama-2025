0611231008aa5f2c6892948409a698330a619620
coskunesen <natuvanem3@gmail.com>
2025-10-21 23:35:35 +0300

    Refactor income tax calculation and output format
    Aylık Net Gelir hesabı eklendi

--- main.py
+++ main.py
@@ -16,32 +16,51 @@ v4300x=(4300000-800000)*0.35
 if bordro:
     if gelir<158000:
         vergi=gelir*0.15
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<330000:
         vergi=v158+((gelir-158000)*0.20)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<1200000:
         vergi=v158+v330+((gelir-330000)*0.27)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<4300000:
         vergi=v158+v330+v1200+((gelir-1200000)*0.35)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     else:
         vergi=v158+v330+v1200+v4300+((gelir-4300000)*0.40)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
 else:
     if gelir<158000:
         vergi=gelir*0.15
+        ayliknetgelir=gelir-vergi
+        if  gelir>0 :  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<330000:
         vergi=v158+((gelir-158000)*0.20)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<800000:
         vergi=v158+v330+((gelir-330000)*0.27)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     elif gelir<4300000:
         vergi=v158+v330+v800+((gelir-800000)*0.35)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
     else:
         vergi=v158+v330+v800+v4300x+((gelir-4300000)*0.40)
+        ayliknetgelir = gelir - vergi
+        if gelir > 0:  st.write("Aylık Net Gelir: ", ayliknetgelir)
 
 
 net=gelir-vergi
 ay=gelir/12
 aynet=net/12
 
-print("Yıllık Gelir",gelir,"Yıllık Net Gelir",net,"Aylık Brüt Gelir",ay,"Aylık Net Gelir",aynet,"Toplam Vergi",vergi)
-st.write(" Yıllık Gelir",gelir,"Yıllık Net Gelir",net,"Aylık Brüt Gelir",round(ay,2),"Aylık Net Gelir",round(aynet,2),"Toplam Vergi",vergi)
-st.write("Hayırlı Bereketli Kazançlar")
+print("Yıllık Gelir",gelir,"Yıllık Net Gelir",net,"Aylık Brüt Gelir",ay,"Aylık Ortalama Net Gelir",aynet,"Toplam Vergi",vergi)
 
+st.write(" Yıllık Gelir",gelir,"Yıllık Net Gelir",net,"Aylık Brüt Gelir",round(ay,2),"Aylık Ortalama Net Gelir",round(aynet,2),"Toplam Vergi",vergi)
